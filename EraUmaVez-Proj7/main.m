c4063d37a1119b25ffa4dc4c024a0eb54b79b65b
LEONARDO DE SOUSA MENDES <leonardo.smendes1@CAS390285M441.local>
2014-03-13 14:45:29 -0300

    Initial Commit

---
+++ EraUmaVez-Proj7/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EraUmaVez-Proj7
+//
+//  Created by LEONARDO DE SOUSA MENDES on 13/03/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
